68324219f6e419a25f32c1885f315bade7f52dbe
TeslaSTEMCSTeacher <bkankelborg@lwsd.org>
2022-03-03 08:29:45 -0800

    Populating starter project: Part 2

---
+++ TextBattlePart1/src/Item.java
@@ -0,0 +1,15 @@
+/**
+ * Name: 
+ * Last Updated On: 
+ * Mrs. Kankelborg
+ * APCS Period 
+ * Text Battle Project Part One
+ * 
+ * This class represents an Item object. It must contain all of the fields and
+ * methods detailed in the project spec. You may add additional fields and methods if you
+ * like.
+ */
+
+public class Item {
+
+}
