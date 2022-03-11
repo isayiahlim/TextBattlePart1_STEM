3484c826d726c23dcc0fa87bcc51c1c8142f6eb3
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2022-03-10 22:54:16 -0800

    altering index

--- TextBattlePart1/src/Player.java
+++ TextBattlePart1/src/Player.java
@@ -136,7 +136,8 @@ public class Player
 	//uses an item
 	public void useItem(int index)
 	{
-		if(index < 0 || index > inventory.length - 1)
+		index --;
+		if(index < -1 || index >= inventory.length)
 			System.out.println("Invalid Selection - Missed Turn\n");
 		else if(inventory[index] == null)
 			System.out.println("Invalid Selection - Missed Turn\n");
