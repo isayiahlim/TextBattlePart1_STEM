7c91be7060bfc0b608ab10d2b393c03a40e3850b
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2022-03-11 00:34:06 -0800

    Undoing it

--- TextBattlePart1/src/Player.java
+++ TextBattlePart1/src/Player.java
@@ -136,15 +136,15 @@ public class Player
 	//uses an item
 	public void useItem(int index)
 	{
-		index++;
-		if(index <= 0 || index > inventory.length)
+		
+		if(index < 0 || index > inventory.length)
 			System.out.println("Invalid Selection - Missed Turn\n");
-		else if(inventory[index-2] == null)
+		else if(inventory[index-1] == null)
 			System.out.println("Invalid Selection - Missed Turn\n");
 		else
 		{
-			inventory[index-2].use(this);
-			inventory[index-2] = null;
+			inventory[index-1].use(this);
+			inventory[index-1] = null;
 		}
 	}
 	
