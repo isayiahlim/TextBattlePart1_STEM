e275778f096307ba10b5f2585d108551924b0876
s-ILIM <s-ILIM@487M113743.lwsd.k12wa.org>
2022-03-11 01:24:44 -0800

    Made the index change.

--- TextBattlePart1/src/Player.java
+++ TextBattlePart1/src/Player.java
@@ -136,7 +136,7 @@ public class Player
 	//uses an item
 	public void useItem(int index)
 	{
-		index--;
+		index = index - 1;
 		if(index < 0 || index >= inventory.length)
 			System.out.println("Invalid Selection - Missed Turn\n");
 		else if(inventory[index] == null)
